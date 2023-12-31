418a396bb26e788b8e94ce45d1f261f4658760d6
Daniel Bonam <daniiel.bonam@gmail.com>
2023-12-30 23:51:42 -0300

    EnumProcesses

---
+++ EnumProcesses/README.md
@@ -0,0 +1,8 @@
+# EnumProcesses
+Lista os processos do sistema utilizando a chamada EnumProcesses.
+
+## Como funciona?
+Em resumo, utilizamos a chamada EnumProcesses para obtermos todos os PID's em execução no momento da chamada. Com os PID's em mãos, abrimos cada uma dos processos e verificamos o seu PATH.
+
+### Observação
+Como o código abre o processo dentro de um handler para obter as informações, não é possivel obter o path de processos protegidos pelo sistema. Mais informações sobre permissões e acesso clique [aqui.](https://learn.microsoft.com/en-us/windows/win32/procthread/process-security-and-access-rights)
--- EnumProcesses/main.c
+++ EnumProcesses/main.c
@@ -1,27 +1,64 @@
 #include <windows.h>
+#include <stdio.h>
+#include <psapi.h>
+
+int getNameByPid(DWORD dwProcessId);
 
 int main(void)
 {
-    DWORD aProcesses[1024]; //  ARRAY PARA ARMAZENAR OS PIDS
-    DWORD cbNeeded;         //  ARMAZENA O NUMERO DE BYTES RETORNADOS EM aProcesses
-    DWORD cProcesses;       //  ARMAZENA O NUMERO DE PROCESSOS ENUMERADOS (NUMERO TOTAL DE BYTES / TAMANHO DE CADA ARRAY)
-    int i;                  //  VARIAVEL PARA O CONTADOR UTILIZADO AO EXIBIR AS INFORMACOES DOS PROCESSOS OBTIDOS
+    DWORD aProcesses[1024];
+    DWORD cbNeeded;
+    DWORD cProcesses;
+    int i;
+    int FailedProcesEnum = 0;
+    int SucessProcesEnum = 0;
 
-    if ( !EnumProcesses( aProcesses, sizeof(aProcesses), &cbNeeded ) )
+    if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
     {
+        printf("[-] Failed to enum processes.\n%d", GetLastError());
         return EXIT_FAILURE;
     }
     printf("[+] Sucess to enum processes.\n");
 
-    cProcesses = cbNeeded / sizeof(DWORD);  //  DIVIDIMOS POR DWORD POIS ELEMENTO ARMAZENADO NO ARRAY E DESTE TIPOP
-    
-    for ( i = 0; i < cProcesses; i++ )
+    cProcesses = cbNeeded / sizeof(DWORD);
+
+    for (i = 0; i < cProcesses;i++)
     {
-        if ( aProcesses[i] != 0)
+        if (aProcesses[i] != 0)
         {
-            PrintProcessNameAndId( aProcesses[i] );
+            if(!getNameByPid(aProcesses[i]))
+            {
+                SucessProcesEnum++;
+            }
+            FailedProcesEnum++;
         }
     }
 
+    printf("----------\n");
+    printf("Sucess processes: %d\n", SucessProcesEnum);
+    printf("Faild processes: %d\n", FailedProcesEnum);
+    printf("Total: %d\n", (FailedProcesEnum + SucessProcesEnum));
+    return EXIT_SUCCESS;
+}
+
+int getNameByPid(DWORD dwProcessId)
+{
+    HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, dwProcessId);
+    WCHAR szProcessPath[MAX_PATH];
+
+    if (hProcess == NULL)
+    {
+        printf("[-] PID: %d\t-\tFailed to open process. Error: %d\n", dwProcessId, GetLastError());
+        return EXIT_FAILURE;
+    }
+
+    if(!GetProcessImageFileNameW(hProcess, szProcessPath, MAX_PATH))
+    {
+		printf("[-] PID: %d\t-\tERROR: %d\n", dwProcessId, GetLastError());
+        return EXIT_FAILURE;
+    }
+    printf("[+] PID: %d\t-\tPATH: %S\n", dwProcessId, szProcessPath);
+
+    CloseHandle(hProcess);
     return EXIT_SUCCESS;
 }
